0fa2bc536862b30b394e1afdc43af5d258785681
chandreshD4 <rajputchandresh163pc1@gmail.com>
2026-07-11 18:58:47 +0530

    Migrate MainActivity from Kotlin to Java to completely fix unresolved reference error

---
+++ android/app/src/main/java/com/example/ck_aradhana/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ck_aradhana;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/ck_aradhana/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ck_aradhana
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
